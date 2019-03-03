3e283765e1be0503727b7163b214c3102b0198c9
mxlFighting <1141068519@qq.com>
2019-03-03 09:41:35 +0800

    第一天的第二次提交完成了首页的布局和功能

---
+++ public/m/js/index.js
@@ -0,0 +1,28 @@
+$(function () {
+    //1. mui 自动轮播初始化js
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+        interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
+    });
+
+    // 小圆点 js
+
+    mui.init({
+        swipeBack: true //启用右滑关闭功能
+    });
+
+    //2. 区域滚动js
+    options = {
+        scrollY: true, //是否竖向滚动
+        scrollX: false, //是否横向滚动
+        startX: 0, //初始化时滚动至x
+        startY: 0, //初始化时滚动至y
+        indicators: true, //是否显示滚动条
+        deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏
+        bounce: true //是否启用回弹
+    }
+    //初始化
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+})
